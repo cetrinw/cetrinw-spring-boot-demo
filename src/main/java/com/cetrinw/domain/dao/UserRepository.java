962da161e7487fbcc528ddae564720ecd0326caa
cetrinw <cetrinw@sina.com>
2017-02-22 10:12:31 +0800

    jpa实现

---
+++ src/main/java/com/cetrinw/domain/dao/UserRepository.java
@@ -0,0 +1,25 @@
+package com.cetrinw.domain.dao;
+
+import com.cetrinw.domain.entity.BootUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by Cetrin Wang on 2017/2/21.
+ */
+public interface UserRepository extends JpaRepository<BootUser, String> {
+//    List<BootUser> findByAge(String name);
+//
+//    BootUser findById(String id);
+//
+//    @Query("select u from BootUser u where u.id = :id")
+//    BootUser query(@Param("id") String id);
+//
+//    /**
+//     * 与实体类对应
+//     * @param id
+//     * @return
+//     */
+//    BootUser namedQuery(String id);
+
+}
